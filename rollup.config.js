c916b7b442b2ae304bc4f6fc00b6cde46c625ea4
Mathieu Debove <mathieu@debove.dev>
2020-08-09 21:03:47 +0200

    Add Typescript support

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
 import pkg from './package.json'
 
 const version = process.env.VERSION || require('./package.json').version
@@ -12,19 +14,31 @@ const banner =
 const name = 'caassis-vue-keycloak'
 
 // CommonJS (for Node), ES module (for bundlers) and browser-friendly UMD build.
-export default {
-  input: 'src/index.js',
-  output: [
-    { file: pkg.main, format: 'cjs', banner, name },
-    { file: pkg.module, format: 'es', banner, name },
-    { file: pkg.browser, format: 'umd', banner, name }
-  ],
-  plugins: [
-    resolve(), // so Rollup can find `keycloak-js`
-    commonjs(), // so Rollup can convert `keycloak-js` to an ES module
-    babel({
-      exclude: ['node_modules/**']
-    })
-  ]
-}
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'named', banner, name },
+      { file: pkg.module, format: 'es', exports: 'named', banner, name },
+      { file: pkg.browser, format: 'umd', exports: 'named', banner, name }
+    ],
+    plugins: [
+      resolve(), // so Rollup can find `keycloak-js`
+      commonjs(), // so Rollup can convert `keycloak-js` to an ES module
+      typescript()
+    ]
+  },
+  {
+    input: 'src/types.d.ts',
+    output: [ {file: 'dist/types.d.ts', format: 'es'} ],
+    plugins: [
+      commonjs(),
+      babel({
+        exclude: ['node_modules/**']
+      }),
+      typescript(),
+      dts()
+    ]
+  }
+]
 
